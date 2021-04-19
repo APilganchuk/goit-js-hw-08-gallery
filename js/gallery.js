3458f25c2a92dd70950e6876b282879d366ce82a
APilganchuk <opilganchuk@gmail.com>
2021-04-19 16:17:42 +0300

    done

--- js/gallery.js
+++ js/gallery.js
@@ -5,6 +5,7 @@ const refs = {
     modal: document.querySelector('.js-lightbox'),
     modalOverlay: document.querySelector('.lightbox__overlay'),
     modalContent: document.querySelector('.lightbox__content'),
+    currentImg: document.querySelector('.lightbox__image'),
     OnBtnCloseModal: document.querySelector('[data-action="close-lightbox"]'),
 };
 
@@ -29,16 +30,41 @@ const makeGalleryImagesMarkup = images.map(makeGalleryImageMarkup).join('');
 refs.gallery.insertAdjacentHTML('afterbegin', makeGalleryImagesMarkup);
 
 refs.gallery.addEventListener('click', onModalOpen);
-refs.OnBtnCloseModal.addEventListener('click', onModalClose);
 
 function onModalOpen(evt) {
-    if (evt.target.classList.contains('gallery__link')) {
+    if (!evt.target.classList.contains('gallery__image')) {
         return;
     }
-    refs.gallery.nextElementSibling.classList.add('is-open');
-    
+    refs.modal.classList.add('is-open');
+
+    showCurrentImage(evt);
+    // слушатели событий для закрытия мод.окна разными способами
+    refs.OnBtnCloseModal.addEventListener('click', onModalClose);
+    refs.modalOverlay.addEventListener('click', closeModalOnClickOverlay);
+    window.addEventListener('keydown', closeModalOnPressEsc);
+}
+
+function showCurrentImage(evt) {
+    refs.currentImg.src = evt.target.dataset.source;
+    refs.currentImg.alt = evt.target.alt;
 }
 
 function onModalClose() {
-    refs.gallery.nextElementSibling.classList.remove('is-open');
+    refs.modal.classList.remove('is-open');
+    removeAttrCurrentImg();
+}
+function removeAttrCurrentImg() {
+    refs.currentImg.src = '';
+    refs.currentImg.alt = '';
+}
+function closeModalOnClickOverlay() {
+    onModalClose();
+}
+function closeModalOnPressEsc(evt) {
+    if (evt.code !== 'Escape') {
+        return;
+    }
+    onModalClose();
+    window.removeEventListener('keydown', closeModalOnPressEsc);
+    
 }
